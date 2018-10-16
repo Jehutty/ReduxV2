151a04fd80ba40728bdf079b3039a74691a1ac17
jehutty <athsivridis@gmail.com>
2018-10-16 19:55:15 +0300

    asd

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Javascript: the good parts'},
+        {title: 'Harry potter'},
+        {title: 'Lord of the rings'},
+        {title: 'Dark tower'}
+    ]
+}
