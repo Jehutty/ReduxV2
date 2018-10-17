55acd0a2f032b656a67e73e4a6798e76c4db738a
jehutty <athsivridis@gmail.com>
2018-10-17 21:39:05 +0300

    Final Commit

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        {title: 'Javascript: the good parts'},
-        {title: 'Harry potter'},
-        {title: 'Lord of the rings'},
-        {title: 'Dark tower'}
+        {title: 'Javascript: the good parts', pages:"101"},
+        {title: 'Harry potter', pages: "3540"},
+        {title: 'Lord of the rings',pages:"3434"},
+        {title: 'Dark tower', pages:"1"}
     ]
 }
